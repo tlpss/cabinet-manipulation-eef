39dfa4d19d8bf5f9886e61453a5937c5afcd989a
tlips <thomas17.lips@gmail.com>
2023-02-07 15:11:23 +0100

    update dockerfile

--- ROS/scripts/ure-fzi-start-script.sh
+++ ROS/scripts/ure-fzi-start-script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 
+# convenience script to run the ros launch file for the Victor UR3e robot.
 source install/setup.bash
 ros2 launch ure_cartesian_controllers ure_fzi_controllers.launch.py ur_type:=ur3e robot_ip:=10.42.0.162 use_fake_hardware:=false launch_rviz:=true
